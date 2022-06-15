95ac25d5d2b0c482bd85498e5d9c65c9693680be
moonstripe <kojinglick@gmail.com>
2022-06-15 00:10:13 -0400

    demo able

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Box sx={{ backgroundColor: 'aliceblue', height: window.innerHeight, justifyContent: 'center' }}>
       <Grid container>
-        <Grid item xs={8}>
+        <Grid item xs={7}>
           <Typography variant={'h2'} sx={{ margin: 1}}>Main page content</Typography>
           <Typography variant={'body1'} sx={{ margin: 1}}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus proin nibh nisl condimentum id venenatis. Fermentum posuere urna nec tincidunt praesent semper feugiat. Consectetur a erat nam at lectus urna duis convallis convallis. Elementum eu facilisis sed odio morbi quis. Volutpat est velit egestas dui id ornare arcu odio ut. Sit amet aliquam id diam maecenas. Nisi est sit amet facilisis magna etiam tempor orci eu. Enim nulla aliquet porttitor lacus. At varius vel pharetra vel. Sit amet facilisis magna etiam tempor orci eu. A pellentesque sit amet porttitor eget dolor morbi non arcu.
@@ -18,9 +18,9 @@ function App() {
             Ultrices in iaculis nunc sed augue lacus viverra. Urna et pharetra pharetra massa massa. Quis eleifend quam adipiscing vitae proin sagittis nisl. Urna condimentum mattis pellentesque id nibh tortor id. Pulvinar neque laoreet suspendisse interdum consectetur libero. Sit amet risus nullam eget. Eget dolor morbi non arcu. Vel pretium lectus quam id leo in vitae turpis massa. Auctor elit sed vulputate mi sit amet mauris commodo. In egestas erat imperdiet sed. Porta lorem mollis aliquam ut porttitor.
           </Typography>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={5}>
           <Box sx={{ m: 1 }}>
-            <Typography variant={'h2'}>In the News</Typography>
+            {/* <Typography variant={'h2'}>In the News</Typography> */}
             <VertCarousel autoScroll/>
           </Box>
         </Grid>
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,36 +1,87 @@
 import React, { useState, useEffect } from 'react'
-import { Card, CardContent, Typography, Box, CardActionArea, Grid } from '@mui/material';
-import { Circle, CircleOutlined } from '@mui/icons-material';
+import { Card, CardContent, CardMedia, Typography, Box, CardActionArea, Grid } from '@mui/material';
+import { Circle, CircleOutlined, ArrowUpwardRounded, ArrowDownwardRounded } from '@mui/icons-material';
 import { motion, useAnimation } from 'framer-motion'
 
 export const VertCarousel = (props) => {
-    const initialMessageArr = [{
-        id: 0,
-        title: "hello",
-        text: "this is the first item in the carousel"
-    }, {
-        id: 1,
-        title: "what's up",
-        text: "this is the second item in the carousel"
-    }, {
-        id: 2,
-        title: "howdy",
-        text: "this is the third item in the carousel"
-    }, {
-        id: 3,
-        title: "pip pip",
-        text: "this is the fourth item in the carousel"
-    }, {
-        id: 4,
-        title: "goodbye",
-        text: "this is the fifth item in the carousel"
-    }]
+    // const initialMessageArr = [{
+    //     id: 0,
+    //     title: "The Privacy Engineer's Manifesto",
+    //     image: "https://images-na.ssl-images-amazon.com/images/I/41WkDYRFxyL._SX348_BO1,204,203,200_.jpg",
+    //     text: "The first book of its kind, offering industry-proven solutions",
+    //     link: "https://www.amazon.com/Privacy-Engineers-Manifesto-Getting-Policy/dp/1430263555"
+    // }, {
+    //     id: 1,
+    //     title: "5 Ways to Protect Yourself",
+    //     image: "https://miro.medium.com/fit/c/176/176/1*0VZbtd0GNGfjG_7HXqFK6w.png",
+    //     text: "As a consumer, there are many things you can do to protect yourself",
+    //     link: "https://medium.com/berkeleyischool/5-ways-to-protect-yourself-from-a-cyberattack-2d09a5b9fb51"
+    // }, {
+    //     id: 2,
+    //     title: "Smarter Markets: Michelle Dennedy",
+    //     image: "https://pbcdn1.podbean.com/imglogo/image-logo/10226384/UPDATED_RSS_POD_LOGO_pz94dw.png",
+    //     text: "Erik Townsend interviews new Smarter Markets host Michelle Dennedy",
+    //     link: "https://smartermarkets.podbean.com/e/michelle-dennedy-crusading-to-protect-digital-privacy/"
+    // }, 
+    // {
+    //     id: 3,
+    //     title: "Private Market Trends of 2022",
+    //     image: "https://carta.com/events/wp-content/uploads/sites/3/2022/01/Private-Market-Trends-2022-2.png",
+    //     text: "Join Michelle in discussion with CARTA",
+    //     link: "https://carta.com/events/private-market-trends-of-2022/"
+    // },
+    // {
+    //     id: 4,
+    //     title: "Podcast: Social Media & Society",
+    //     image: "https://pbcdn1.podbean.com/imglogo/image-logo/10226384/UPDATED_RSS_POD_LOGO_pz94dw.png",
+    //     text: "Michelle Dennedy interviews Kirstine Stewart of the World Economic Forum",
+    //     link: "https://smartermarketspod.com/kirstine-stewart-head-of-media-sport-entertainment-for-the-world-economic-forum/"
+    // }]
+
+    const initialMessageArr = [
+        {
+            id: 0,
+            title: "First Item",
+            text: "The descriptions should not exceed one or maybe two lines.",
+            image: "https://images-na.ssl-images-amazon.com/images/I/41WkDYRFxyL._SX348_BO1,204,203,200_.jpg",
+            link: "https://www.amazon.com/Privacy-Engineers-Manifesto-Getting-Policy/dp/1430263555"
+        },
+        {
+            id: 1,
+            title: "Second Item",
+            text: "The descriptions should not exceed one or maybe two lines.",
+            image: "https://images-na.ssl-images-amazon.com/images/I/41WkDYRFxyL._SX348_BO1,204,203,200_.jpg",
+            link: "https://www.amazon.com/Privacy-Engineers-Manifesto-Getting-Policy/dp/1430263555"
+        },
+        {
+            id: 2,
+            title: "Third Item",
+            text: "The descriptions should not exceed one or maybe two lines.",
+            image: "https://images-na.ssl-images-amazon.com/images/I/41WkDYRFxyL._SX348_BO1,204,203,200_.jpg",
+            link: "https://www.amazon.com/Privacy-Engineers-Manifesto-Getting-Policy/dp/1430263555"
+        },
+        {
+            id: 3,
+            title: "Fourth Item",
+            text: "The descriptions should not exceed one or maybe two lines.",
+            image: "https://images-na.ssl-images-amazon.com/images/I/41WkDYRFxyL._SX348_BO1,204,203,200_.jpg",
+            link: "https://www.amazon.com/Privacy-Engineers-Manifesto-Getting-Policy/dp/1430263555"
+        },
+        {
+            id: 4,
+            title: "Fifth Item",
+            text: "The descriptions should not exceed one or maybe two lines.",
+            image: "https://images-na.ssl-images-amazon.com/images/I/41WkDYRFxyL._SX348_BO1,204,203,200_.jpg",
+            link: "https://www.amazon.com/Privacy-Engineers-Manifesto-Getting-Policy/dp/1430263555"
+        }
+    ]
 
     const { autoScroll } = props
     const [activeMessageArr, setActiveMessageArr] = useState(initialMessageArr)
     const [prev, setPrev] = useState(false)
     const [first, setFirst] = useState(0)
     const [timer, setTimer] = useState(autoScroll)
+    const [isScrolling, setIsScrolling] = useState(false)
     const controls = useAnimation()
 
     // opacity function
@@ -38,8 +89,9 @@ export const VertCarousel = (props) => {
 
     const offset = (arr, offset) => [...arr.slice(offset), ...arr.slice(0, offset)];
 
-    const handleNext = async (e, k) => {
+    const handleNext = async () => {
         setPrev(false)
+        setIsScrolling(true)
 
         let next = first + 1
 
@@ -55,7 +107,7 @@ export const VertCarousel = (props) => {
             y: -108,
             opacity: i === 0 ? 0 : Math.exp(-(i - 1) / 2),
             transition: {
-                duration: 1
+                duration: 0.3
             }
         }))
 
@@ -72,13 +124,13 @@ export const VertCarousel = (props) => {
         } else {
             setFirst(next % initialMessageArr.length)
         }
-
+        setIsScrolling(false)
         return controls.stop
-
     }
 
     const handlePrev = async () => {
         setPrev(true)
+        setIsScrolling(true)
 
         let prev = first - 1
 
@@ -108,10 +160,10 @@ export const VertCarousel = (props) => {
             y: 0,
             opacity: i === initialMessageArr.length ? 0 : Math.exp(-i / 2),
             transition: {
-                duration: 1
+                duration: 0.3
             }
         }))
-
+        setIsScrolling(false)
         return controls.stop
     }
 
@@ -127,14 +179,14 @@ export const VertCarousel = (props) => {
         if (autoScroll && timer) {
             const interval = setInterval(() => {
                 handleNext();
-            }, 1500000);
+            }, 3000);
             return () => clearInterval(interval);
         }
-    }, [timer])
+    }, [timer, first])
 
     return (
         <Box
-            sx={{ display: 'flex', flexDirection: 'column', alignItems: 'stretch', justifyContent: 'center', maxHeight: '80vh', width: 'inherit', zIndex: "-3" }}
+            sx={{ display: 'flex', flexDirection: 'column', mt: 4, alignItems: 'stretch', justifyContent: 'center', maxHeight: '80vh', width: 'inherit' }}
             onMouseEnter={() => turnOffTimer()}
             onMouseLeave={() => turnOnTimer()}
         >
@@ -151,60 +203,72 @@ export const VertCarousel = (props) => {
                                 <CircleOutlined
                                     key={i}
                                     sx={{ pl: 1, width: '0.75em' }}
-                                    onClick={() => {
+                                    onClick={!isScrolling ? () => {
                                         setActiveMessageArr(offset(initialMessageArr, i))
                                         setFirst(i)
-                                    }}
+                                    } : null}
                                 />
                         ))
                     }
                 </Grid>
                 <Grid item xs={11}>
                     <Card
-                        onClick={handlePrev}
-                        sx={{ mb: 1, maxWidth: 345, height: 32, mx: 1, zIndex: '1' }}
+                        onClick={!isScrolling ? handlePrev : null}
+                        sx={{ display: 'flex', flexDirection: 'row', mb: 1, maxWidth: 345, maxHeight: "100px", mx: 1, opacity: 0.5, zIndex: '1', alignContent: 'center', justifyContent: 'center' }}
                     >
-                        <CardContent>
-                            <Typography gutterBottom variant="h6" component="div">
-                                
-                            </Typography>
-                        </CardContent>
+                        <ArrowUpwardRounded
+                            sx={{ mt: 0.5 }}
+                        />
                     </Card>
                     {
                         offset(initialMessageArr, first).map((e, i) => (
                             i < 4 ? <Card
                                 key={e.id}
-                                sx={{ height: "100px", maxWidth: 345, mx: 1, mb: 1, opacity: Math.exp(-i / 2), zIndex: -1 }}
+                                sx={{ height: "auto", maxWidth: 345, mx: 1, mb: 1, opacity: Math.exp(-i / 2), zIndex: -1 }}
                                 custom={i}
                                 initial={prev ? {
                                     y: -108,
                                     opacity: 0
-                                } : {}}
+                                } : {
+                                }}
                                 component={motion.div}
                                 animate={controls}
+                                disableRipple={!isScrolling ? false : true}
                             >
-                                <CardActionArea>
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h6" component="div">
-                                            {e.title}
-                                        </Typography>
-                                        <Typography variant="body2" color="text.secondary">
-                                            {e.text}
-                                        </Typography>
-                                    </CardContent>
+                                <CardActionArea 
+                                    href={!isScrolling ? e.link : null}
+                                    target="_blank"
+                                    >
+                                    <Grid container>
+                                        <Grid item xs={3}>
+                                            <CardMedia
+                                                component="img"
+                                                src={e.image}
+                                                sx={{maxHeight: "100px"}}
+                                                 />
+                                        </Grid>
+                                        <Grid item xs={9}>
+                                            <CardContent>
+                                                <Typography gutterBottom variant="subtitle2" component="div">
+                                                    {e.title}
+                                                </Typography>
+                                                <Typography variant="body2" color="text.secondary">
+                                                    {e.text}
+                                                </Typography>
+                                            </CardContent>
+                                        </Grid>
+                                    </Grid>
                                 </CardActionArea>
                             </Card> : null
                         ))
                     }
                     <Card
-                        onClick={handleNext}
-                        sx={{ mb: 1, maxWidth: 345, height: 32, width: 'inherit', mx: 1, zIndex: '1' }}
+                        onClick={!isScrolling ? handleNext : null}
+                        sx={{ display: 'flex', flexDirection: 'row', mb: 1, maxWidth: 345, height: 32, width: 'inherit', mx: 1, opacity: 0.5, zIndex: '1', alignContent: 'center', justifyContent: 'center' }}
                     >
-                        <CardContent>
-                            <Typography gutterBottom variant="h6" component="div">
-                                
-                            </Typography>
-                        </CardContent>
+                        <ArrowDownwardRounded
+                            sx={{ mt: 0.5 }}
+                        />
                     </Card>
                 </Grid>
             </Grid>
